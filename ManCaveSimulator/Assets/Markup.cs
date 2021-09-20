99a1bfb339a37550f35e90c1d7bfecc62d16c59e
liam <lkodo573@learner.dvusd.org>
2021-09-20 09:22:56 -0700

    cue

---
+++ ManCaveSimulator/Assets/Markup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Markup : MonoBehaviour
+{
+    [SerializeField] Camera MainCamera;
+    [SerializeField] GameObject Cue;
+    [SerializeField] GameObject Cue1;
+
+    private void Awake()
+    {
+        Cue.SetActive(false);
+
+    }
+    public void ToggleLinkedObject()
+    {
+        Cue.SetActive(!Cue.activeSelf);
+        Cue1.SetActive(false);
+    }
+}
---
+++ ManCaveSimulator/Assets/RayCast.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCast : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                Markup hasMarkup = hit.transform.gameObject.GetComponent<Markup>();
+                if (hasMarkup != null)
+                {
+                    hasMarkup.ToggleLinkedObject();
+
+                }
+
+            }
+        }
+    }
+}
